30f8bd0f49c87e9b081b6fd4f9526d16dfa48864
Briana Oursler <briana.oursler@gmail.com>
2022-12-07 16:47:01 -0800

    Stat: separate library and binary concerns
    Pull references to structopt, the library used for parsing cli options,
    out of lib/stat.rs and move them into the cli binary. Update code so
    that library no longer relies on structopt, instread receiving its
    parsed output as StatEvents.

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -7,15 +7,7 @@
 //! <li>gui</li>
 //! </ul>
 
-// mod bindings;
-// mod event;
-// mod gui;
-// mod stat;
-// mod test;
-// mod utils;
-
 extern crate structopt;
-//use gui::*;
 use ruperf_profile::stat::*;
 use structopt::StructOpt;
 use ruperf_profile::test::*;
@@ -38,22 +30,29 @@ enum Opt {
         about = "Runs sanity tests"
     )]
     Test(TestOptions),
-    // #[structopt(
-    //     setting = structopt::clap::AppSettings::TrailingVarArg,
-    //     setting = structopt::clap::AppSettings::AllowLeadingHyphen,
-    //     name = "gui",
-    //     about = "Launches gui"
-    // )]
-    //Gui(GuiOptions),
+}
+
+/// Configuration settings for running stat. A program to profile is a required
+/// argument. Default events will run on that program if no events are
+/// specified. Specify events using the flag `-e or --event`. See `./ruperf stat
+/// --help' for more information.
+#[derive(Debug, StructOpt)]
+pub struct StatOptions {
+    #[structopt(short, long, help = "Event to collect", number_of_values = 1)]
+    pub event: Vec<StatEvent>,
+
+    // Allows multiple arguments to be passed, collects everything remaining on
+    // the command line
+    #[structopt(required = true, help = "Command to run")]
+    pub command: Vec<String>,
 }
 
 fn main() {
     let opt = Opt::from_args();
     match opt {
-        Opt::Stat(x) => run_stat(x),
+        Opt::Stat(x) => {
+            run_stat(x.event, x.command);
+        },
         Opt::Test(x) => run_test(&x),
-        // Opt::Gui(x) => {
-        //     stat::run_gui(&x).unwrap();
-        // }
     }
 }
